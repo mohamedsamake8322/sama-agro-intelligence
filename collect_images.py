a111bb119f2364d5b21c603266a73970686ab497
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-07-07 08:56:32 +0300

    jh

--- collect_images.py
+++ collect_images.py
@@ -1,14 +1,15 @@
-from duckduckgo_search import ddg_images
+from duckduckgo_search import DDGS
 import os, urllib.request
 
 def download_images(query, folder, max_images=100):
     os.makedirs(folder, exist_ok=True)
-    results = ddg_images(query, max_results=max_images)
-    for i, result in enumerate(results):
-        try:
-            urllib.request.urlretrieve(result['image'], f"{folder}/{i+1}.jpg")
-        except:
-            continue
+    with DDGS() as ddgs:
+        results = ddgs.images(query, max_results=max_images)
+        for i, result in enumerate(results):
+            try:
+                urllib.request.urlretrieve(result["image"], f"{folder}/{i+1}.jpg")
+            except:
+                continue
 
 categories = {
     "water_stress": "drought stress in crops",
